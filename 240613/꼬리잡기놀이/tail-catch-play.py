0a66ce83f639d7d9fc9a727397467e40fe4851be
yeongleej <81922339+yeongleej@users.noreply.github.com>
2024-06-13 02:17:25 +0900

    Title: 꼬리잡기놀이, Time: 150ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240613/꼬리잡기놀이/tail-catch-play.py
@@ -0,0 +1,133 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
+rx = [1, 0, -1, 0]
+ry = [0, 1, 0, -1]
+
+n, m, k = map(int, input().split())
+g = [list(map(int, input().split())) for _ in range(n)]
+teams = [[] for _ in range(m)]
+visited = [[False]*n for _ in range(n)]
+turn = [[0, 0], [n-1, 0], [n-1, n-1], [0, n-1]]
+
+# 공을 잡은 팀의 머리
+hx, hy = 0, 0
+
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+# bfs
+def find_team(tNum, sx, sy):
+    q = deque([(sx, sy)])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if in_range(nx, ny) and not visited[nx][ny]:
+                if g[nx][ny] == 2:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+                elif g[nx][ny] == 3:
+                    teams[tNum].append([nx, ny])
+                    break
+
+def move():
+    for t in range(m):
+        s = teams[t][0]
+        e = teams[t][1]
+        # 머리 이동
+        for i in range(4):
+            nx = s[0] + dx[i]
+            ny = s[1] + dy[i]
+            if in_range(nx, ny) and g[nx][ny] == 4:
+                g[nx][ny] = 1
+                g[s[0]][s[1]] = 2
+                teams[t][0][0] = nx
+                teams[t][0][1] = ny
+                break
+        # 꼬리 이동
+        for i in range(4):
+            nx = e[0] + dx[i]
+            ny = e[1] + dy[i]
+            if in_range(nx, ny) and g[nx][ny] == 2:
+                g[nx][ny] = 3
+                g[e[0]][e[1]] = 4
+                teams[t][1][0] = nx
+                teams[t][1][1] = ny
+
+# dfs
+def get_pos(x, y, depth):
+    global ans, visited, hx, hy
+    if g[x][y] == 1:
+        # print(depth)
+        ans += depth*depth
+        hx, hy = x, y
+        return 
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if in_range(nx, ny) and not visited[nx][ny] and (g[nx][ny] == 2 or g[nx][ny] == 1):
+            visited[nx][ny] = True
+            get_pos(nx, ny, depth+1)
+            visited[nx][ny] = False
+
+
+def ball_game(sx, sy, d):
+    global visited
+    for i in range(n):
+        nx = sx + i*dx[d]
+        ny = sy + i*dy[d]
+        if in_range(nx, ny) and 1 <= g[nx][ny] <= 3:
+            visited = [[False]*n for _ in range(n)]
+            visited[nx][ny] = True
+            # print(nx, ny)
+            get_pos(nx, ny, 1)
+
+            # 머리랑 꼬리 변경(방향 전환)
+            for j in range(m):
+                team = teams[j]
+                h = team[0]
+                if h[0] == hx and h[1] == hy:
+                    teams[j][0], teams[j][1] = teams[j][1], teams[j][0]
+                    break
+            break
+        
+
+
+t = 0
+for i in range(n):
+    for j in range(n):
+        if g[i][j] == 1:
+            visited[i][j] = True
+            teams[t].append([i, j])
+            find_team(t, i, j)
+            t += 1
+
+r = 1
+ts = 0
+td = 0
+sx, sy = turn[ts]
+d = 0
+isTurn = False
+ans = 0
+while r <= k:
+    if r % n == 0:
+        isTurn = True
+    move()
+    ball_game(sx, sy, d)
+    r += 1
+    if isTurn:
+        ts = (ts + 1) % 4
+        td = (td + 1) % 4
+        d = (d + 1) % 4
+        sx, sy = turn[ts]
+        isTurn = False
+    else:
+        sx = sx + rx[td]
+        sy = sy + ry[td]
+
+print(ans)
