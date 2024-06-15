82e393133be9a6db10fcd48befd726b4090ff28e
yeongleej <81922339+yeongleej@users.noreply.github.com>
2024-06-15 14:51:36 +0900

    Title: 코드트리 투어, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240615/코드트리 투어/codetree-tour.py
+++ 240615/코드트리 투어/codetree-tour.py
@@ -7,6 +7,7 @@ g = []
 distance = []
 isMade = [False]*30005 # 여행상품이 만들어진적 있는지 저장합니다.
 isCancel = [False]*30005  # 여행상품이 취소되었는지 저장합니다.
+sList = [False]*30005
 h = []
 N, M = 0, 0
 
@@ -56,13 +57,15 @@ def dijkstra(start):
 
 
 def make_r(idx, r,dest):
-    isMade[idx] = True
+    # isMade[idx] = True
+    sList[idx] = True
     profit = r - distance[dest]
     heapq.heappush(h, rev(idx, r, dest, profit))
 
 def remove_r(idx):
-    if isMade[idx]:
-        isCancel[idx] = True
+    # if isMade[idx]:
+    #     isCancel[idx] = True
+    sList[idx] = False
 
 def sell():
     while h:
@@ -70,7 +73,9 @@ def sell():
         if p.profit < 0:
             break
         heapq.heappop(h)
-        if not isCancel[p.idx]:
+        # if not isCancel[p.idx]:
+        #     return p.idx
+        if sList[p.idx]:
             return p.idx
     return -1
 
