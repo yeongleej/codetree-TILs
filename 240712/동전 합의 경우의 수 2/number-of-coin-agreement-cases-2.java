6d0dca2e118f8641dbab0760779912a3eff58af9
yeongleej <81922339+yeongleej@users.noreply.github.com>
2024-07-12 15:00:08 +0900

    Title: 동전 합의 경우의 수 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240712/동전 합의 경우의 수 2/number-of-coin-agreement-cases-2.java
+++ 240712/동전 합의 경우의 수 2/number-of-coin-agreement-cases-2.java
@@ -16,23 +16,24 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         coins = new int[N+1];
-        dp = new int[K+1];
         for(int i=1; i<N+1; i++) {
             st = new StringTokenizer(br.readLine());
             coins[i] = Integer.parseInt(st.nextToken());
+        }
+        dp = new int[K+1];
+        for(int i=1; i<K+1; i++) {
+            dp[i] = 10001;
         }
 
         int ans = Integer.MAX_VALUE;
         for(int i = 1; i<N+1; i++) {
             int v = coins[i];
             for(int j=v; j<K+1; j++) {
-                dp[j] = dp[j-v]+1;
+                dp[j] = Math.min(dp[j], dp[j-v]+1);
             }
             // System.out.println(v+": "+Arrays.toString(dp));
-            ans = Math.min(ans, dp[K]);
         }
-
-        System.out.println(ans);
+        System.out.println(dp[K]);
 
     }
 }
