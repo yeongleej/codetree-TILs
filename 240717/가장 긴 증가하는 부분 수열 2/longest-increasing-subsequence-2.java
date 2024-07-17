6264f73ee4af1c4b444f94f75ada72eac24d5dd4
yeongleej <81922339+yeongleej@users.noreply.github.com>
2024-07-17 15:25:57 +0900

    Title: 가장 긴 증가하는 부분 수열 2, Time: 270ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 240717/가장 긴 증가하는 부분 수열 2/longest-increasing-subsequence-2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static List<Integer> dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // System.out.println(Arrays.toString(arr));
+        dp = new ArrayList<>();
+        dp.add(arr[0]);
+        for(int i=1; i<N; i++) {
+            if(arr[i] > dp.get(dp.size()-1)) {
+                dp.add(arr[i]);
+            }else{
+                int idx = bSearch(arr[i]);
+                dp.set(idx, arr[i]);
+            }
+        }
+
+        System.out.println(dp.size());
+
+    }
+    public static int bSearch(int num) {
+        int s = 0;
+        int e = dp.size()-1;
+        int ans = 0;
+
+        while(s < e) {
+            int mid = (s + e) / 2;
+            if(dp.get(mid) < num) {
+                ans = mid;
+                s = mid +1;
+            } else{
+                e = mid -1;
+            }
+        }
+        return ans;
+    }
+}
