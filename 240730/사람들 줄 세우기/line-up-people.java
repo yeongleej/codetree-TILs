bdd489452a2c5cb2544d8c815bf481687ee9fc19
yeongleej <81922339+yeongleej@users.noreply.github.com>
2024-07-30 20:50:29 +0900

    Title: 사람들 줄 세우기, Time: 106ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240730/사람들 줄 세우기/line-up-people.java
+++ 240730/사람들 줄 세우기/line-up-people.java
@@ -20,54 +20,66 @@ public class Main {
         arr = new int[N+1];
         mrr = new int[M];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<M; i++){
+        for(int i=0; i<M; i++) {
             mrr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int eIdx = 0;
-        int fCnt = 0;
         for(int i=0; i<K; i++) {
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
             int pos = Integer.parseInt(st.nextToken());
-            arr[pos] = n;
-            for(int j=0; j<M; j++){
-                if(n == mrr[j] && eIdx < pos){
-                    eIdx = pos;
-                    fCnt = j;
-                }
-            }
-        }
+            arr[pos] = num;
+        } 
+
         // System.out.println(Arrays.toString(arr));
         // System.out.println(Arrays.toString(mrr));
-        // System.out.println(eIdx+", "+fCnt);
-
-        int empty = 0;
-        for(int i=1; i<eIdx; i++) {
-            if(arr[i] == 0){
-                empty++;
-            } else{
-                for(int j=0; j<M; j++){
-                    if(mrr[j] == arr[i]){
-                        fCnt--;
-                        break;
-                    }
-                }
-            }
-        }
-        int start = 0;
-        if(Math.abs(empty-fCnt) > 0){
-            start = 1;
-        } else{
-            start = eIdx+1;
-        }
         int ans = 0;
-        for(int i=start; i<N+1; i++) {
-            if(arr[i] == 0){
+        for(int i=1; i<N+1; i++) {
+            if(arr[i]==1){
                 ans = i;
                 break;
+            } else if(arr[i]==0){
+                arr[i] = 1;
+                // System.out.println(Arrays.toString(arr));
+                if(isCheck()){
+                    ans = i;
+                    break;
+                }
+                arr[i] = 0;
             }
         }
         System.out.println(ans);
+    }
+    public static boolean isCheck() {
+        // int pre = 1;
+        int[] irr = new int[M];
+        
+        // arr 복사
+        int[] trr = new int[N+1];
+        for(int i=1; i<N+1; i++){
+            trr[i] = arr[i];
+        }
+
+        for(int i=0; i<M; i++) {
+            boolean flag = true;
+            for(int j=1; j<N+1; j++){
+                if(trr[j]==mrr[i]){
+                    irr[i] = j;
+                    break;
+                } else if(trr[j] == 0 && flag){
+                    irr[i] = j;
+                    flag = false;
+                }
+            }
+            trr[irr[i]] = mrr[i];
+        }
+        // System.out.println(Arrays.toString(trr));
+        // System.out.println(Arrays.toString(irr));
+        for(int i=0; i<M-1; i++){
+            if(irr[i] > irr[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 }
