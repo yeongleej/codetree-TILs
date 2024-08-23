8abb2017aca1759df3b8841771db1dcfc9a9a8b9
yeongleej <81922339+yeongleej@users.noreply.github.com>
2024-08-23 14:03:46 +0900

    Title: 2배보다 커지는 수열, Time: 169ms, Memory: 9MB, Status: Passed - CodeTree

--- 240823/2배보다 커지는 수열/a-sequence-greater-than-twice.java
+++ 240823/2배보다 커지는 수열/a-sequence-greater-than-twice.java
@@ -22,13 +22,13 @@ public class Main {
         for(int i=2; i<N+1; i++){
             for(int j=1; j<M+1; j++){
                 for(int k=1; k<=j/2; k++){
-                    dp[i][j] += (dp[i-1][k] % MOD);
+                    dp[i][j] = (dp[i][j] + dp[i-1][k]) % MOD;
                 }
             }
         }
         int ans = 0;
         for(int j=1; j<M+1; j++){
-            ans += (dp[N][j] % MOD);
+            ans = (ans + dp[N][j]) % MOD;
         }
         System.out.println(ans);
     }
